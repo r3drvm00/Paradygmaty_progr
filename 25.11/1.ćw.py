96c83efa025710852a42d0d11e9c93f98942edd6
r3drvm00 <153330612+r3drvm00@users.noreply.github.com>
2023-12-09 00:31:54 +0100

    Add files via upload

---
+++ 25.11/1.ćw.py
@@ -0,0 +1,33 @@
+import unittest
+
+#"""
+#Napisać funkcję (wpisać kod ↓↓), która podaje rozwiązanie następującego zadania:
+#Drapieżnik morski poluje na ryby; z jego rewirze pojawiła się pewna grupa ryb; ich wielkości zadane są
+#listą `fishes`. Wiadomo, że drapieżnik może zjeść tylko ryby o wielkości >= min_size, i <= max_size. 
+#Funkcja powinna obliczyć na ile ryb z grupy drapieżnik może zapolować. 
+
+#"""
+
+
+def eat_small_fish(fishes: list[int], min_size: int, max_size: int) -> int:
+    count = 0
+    for size in fishes:
+        if min_size <= size <= max_size:
+            count += 1
+    return count
+
+
+class TestEngine1(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(eat_small_fish([1, 2, 3, 4, 5], 2, 3), 2)
+
+    def test_2(self):
+        self.assertEqual(eat_small_fish([1, 1, 1, 2, 2], 2, 3), 2)
+
+    def test_3(self):
+        self.assertEqual(eat_small_fish([1, 10], 2, 3), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 25.11/2.ćw.py
@@ -0,0 +1,52 @@
+import unittest
+
+"""
+Napisać funkcję (wpisać kod ↓↓), która podaje rozwiązanie następującego zadania:
+Mamy trzy liczby całkowite, podane w liście "data". Próbujemy stworzyć z tych liczb _kombinację_ 
+o największej sumie, przy czym _kombinacją_ jest tablica podobna do "data", z tym, że pojedyncze jej
+elementy mogą mieć zmieniony znak. 
+
+Przykaład:
+data = [1,-2,3]
+kombinacje:
+[1,-2,3]
+[1,-2,-3]
+[1,2,3]
+[1,2,-3]
+[-1,2,3]
+[-1,2,-3]
+[-1,-2,3]
+[-1,-2,-3]
+
+"""
+
+
+def find_best_combination(data: list[int]) -> int:
+    n = len(data)
+    max_sum = float('-inf')  
+
+   
+    for i in range(2**n):
+        combination = [data[j] if (i & (1 << j)) == 0 else -data[j] for j in range(n)]
+        current_sum = sum(combination)
+
+       
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+class TestEngine2(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(find_best_combination([1, 1, -1]), 3)
+
+    def test_2(self):
+        self.assertEqual(find_best_combination([1, -5, -1]), 7)
+
+    def test_3(self):
+        self.assertEqual(find_best_combination([-2, 0, -1]), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
